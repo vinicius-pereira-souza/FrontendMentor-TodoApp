bea274c5f904acd5d267e96b037e07f786c33917
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2025-01-01 13:13:50 -0300

    config: adicionado as imagens e cores do projeto no tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white font-bold">
-      <h1>Hello World!, This is a first project of 2025</h1>
-    </div>
+    <div
+      className="min-h-screen flex items-center justify-center
+     bg-light-gray-lightest dark:bg-dark-blue-darkest px-6 pt-12 pb-16 lg:pt-20 lg:pb-12 relative"
+    ></div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,33 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "desktop-dark": "url('/images/bg-desktop-dark.jpg')",
+        "desktop-light": "url('/images/bg-desktop-light.jpg')",
+        "mobile-dark": "url('/images/bg-mobile-dark.jpg')",
+        "mobile-light": "url('/images/bg-mobile-light.jpg')",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        light: {
+          gray: {
+            lightest: "#fafafa", // hsl(0, 0%, 98%)
+            lighter: "#ebecf4", // hsl(236, 33%, 92%)
+            light: "#d2d3db", // hsl(233, 11%, 84%)
+            dark: "#9699a3", // hsl(236, 9%, 61%)
+            darkest: "#575a64", // hsl(235, 19%, 35%)
+          },
+        },
+        dark: {
+          blue: {
+            darkest: "#1a1c25", // hsl(235, 21%, 11%)
+            darker: "#252a41", // hsl(235, 24%, 19%)
+            light: "#e4e5f1", // hsl(234, 39%, 85%)
+            hover: "#ebecf4", // hsl(236, 33%, 92%)
+            dark: "#888b91", // hsl(234, 11%, 52%)
+            medium: "#5d606d", // hsl(233, 14%, 35%)
+            darkest2: "#444953", // hsl(237, 14%, 26%)
+          },
+        },
       },
     },
   },

e1d1e5d03ef9516235d1888a66cc4b4b595c2ae8
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2025-01-01 14:18:42 -0300

    styled: css do  formulario finalizado

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,27 @@ import Header from "./ui/header";
 export default function Home() {
   return (
     <div
-      className="min-h-screen flex items-center justify-center
+      className="min-h-screen flex justify-center
      bg-light-gray-lightest dark:bg-dark-blue-darkest px-6 pt-12 pb-16 lg:pt-20 lg:pb-12 relative"
     >
       <div className="w-full h-[200px] lg:h-[300px] bg-mobile-light dark:bg-mobile-dark lg:bg-desktop-light dark:lg:bg-desktop-dark bg-no-repeat bg-center bg-cover absolute top-0 left-0" />
       <main className="flex-1 max-w-[540px] relative">
         <Header />
+        <form action="">
+          <div className="min-h-[60px] rounded-md flex items-center gap-x-6 py-[18px] px-5 bg-white dark:bg-dark-blue-darker">
+            <label
+              className="block w-[23px] h-[23px] rounded-full border border-dark-blue-medium"
+              htmlFor="description"
+            ></label>
+            <input
+              type="text"
+              name="description"
+              id="description"
+              placeholder="Create a new todo..."
+              className="outline-none bg-transparent text-dark-blue-light text-lg leading-none"
+            />
+          </div>
+        </form>
       </main>
     </div>
   );
--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between text-white uppercase font-bold mb-8 lg:mb-10">
+    <header className="flex items-center justify-between text-white uppercase font-bold mb-8 lg:mb-8">
       <h1 className="text-[27px] lg:text-[40px] tracking-[10px] lg:tracking-[15px]">
         Todo
       </h1>

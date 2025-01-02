6967c43387d96338e759c777e2e09b4d17ae158e
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2025-01-02 20:34:29 -0300

    config: configurações inciais do json-server finalizado!

---
+++ app/lib/data.ts
@@ -0,0 +1,4 @@
+// salvar dado -> saveNewTask
+// buscar todos os dados -> fetchAllTasks
+// marcar/descarmar como concluido -> updateTaskCompletionStatus
+// excluir um/mutiplos dados atraves dos ids salvos em um array -> deleteTaskByIds
